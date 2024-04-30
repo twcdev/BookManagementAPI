069a91dd80a59657ee2c8f11d128cf52c42be002
Tawatchai Udompol <twc.dev@gmail.com>
2024-04-30 17:29:00 +0700

    Implement Create and Read operations for books

---
+++ BookManagementAPI/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using BookManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Sample Book", Author = "Author A", Year = 2021, Description = "<p>Description of book</p>" }
+        };
+
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            return books;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+                return NotFound();
+            return book;
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook([FromBody] Book book)
+        {
+            // Automatically assign an ID
+            book.Id = books.Any() ? books.Max(b => b.Id) + 1 : 1;
+            books.Add(book);
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+    }
+}
