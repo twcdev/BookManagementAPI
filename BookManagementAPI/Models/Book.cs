f616deed351dc229210fa859bf47147b5bf078ec
Tawatchai Udompol <twc.dev@gmail.com>
2024-04-30 15:45:33 +0700

    Add Book model with properties

---
+++ BookManagementAPI/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookManagementAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string ISBN { get; set; }  // For hardcopy books
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }  // HTML formatted
+        public int Year { get; set; }
+        public string AudioURL { get; set; }  // For audiobooks
+    }
+}
